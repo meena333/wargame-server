ba0dc18c8fcd9f938a9f54ce0c9cb6bd395e2776
Meenakshi Venkat <meenak.venkat@gmail.com>
2019-08-12 15:20:53 +0200

    Install stream and add some endpoints

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const bodyparser = require('body-parser')
 const Sequelize = require('sequelize')
+const Sse = require('json-sse')
 
 const app = express()
 const corsMiddleware = cors()
-const middleware = bodyparser.json()
+const bodyParserMiddleware = bodyparser.json()
 const port = process.env.PORT || 4000;
 
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
 const db = new Sequelize(databaseUrl)
 
 db.sync({ force: false })
-  .then(() => console.log('Db synced'))
+  .then(() => console.log('Database synced'))
   .catch(console.error)
 
 const Game = db.define('game', {
@@ -37,8 +38,45 @@ Player.belongsTo(Game)
 Player.hasMany(Card)
 Card.belongsToMany(Player, { through: 'PlayerCards' })
 
+const stream = new Sse()
+
 app.use(corsMiddleware)
-app.use(middleware)
+app.use(bodyParserMiddleware)
+
+app.get('/stream', (req, res) => {
+  console.log('All fine')
+  res.send('All fine')
+})
+
+app.post('/game', async (req, res) => {
+  const game = await Game.create(req.body)
+
+  // const channels = await Channel.findAll({
+  //   include: [Message]
+  // })
+
+  // const data = JSON.stringify(channels)
+
+  // stream.updateInit(data)
+  // stream.send(data)
+
+  // response.send(channel)
+}
+)
+
+app.post('/player', async (req, res) => {
+  const player = await Player.create(req.body)
+})
+
+app.put('/game/join/:gameId', async (req, res) => {
+  //Update player.gameId, Update game.status, 
+  //Card.findAll(), cards.map(card => player.addCard(card)), 
+  //player.update({ points: 0 })
+})
+
+app.put('/player/play/:cardId', async (req, res) => {
+  //player.update({ playedId: request.params.cardId }), if (otherPersonPlayed), player.update({ points: player.points + 1, playedId: null }), player.removeCard(cardId?), if (allCardsGone) game.update({ status: ‘done’ })
+})
 
 app.listen(port, () => console.log('Listening on port', port))
 
